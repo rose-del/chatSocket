a7027e1c1a43823b13ddd6b228b172ce91ea9331
Paulo <paulosergio.games1997@gmail.com>
2025-01-13 23:21:45 -0300

    comentando Main

--- src/servidor/Main.java
+++ src/servidor/Main.java
@@ -7,13 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Cria um novo objeto da classe ConfiguradorServidor.
         ConfiguradorServidor configuradorServidor = new ConfiguradorServidor();
+
+        // Vai obter a porta que o responsável pelo servidor digitar.
         int porta = configuradorServidor.obterPorta();
 
+        // Se a porta escolhida for uma porta válida:
         if (porta != -1) {
+
+            // Cria um objeto da classe Servidor, passando a porta como parâmetro.
             Servidor servidor = new Servidor(porta);
-            servidor.iniciar();
-        } else {
+            servidor.iniciar(); // Inicia o servidor.
+        } else {    // Se a porta escolhida for inválida:
             JOptionPane.showMessageDialog(null, "Servidor não iniciado. A configuração foi " +
                     "cancelada.");
         }
