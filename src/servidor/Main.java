3a874417df9cd4ec05ceedff283a8ae0f0fc4f25
Paulo <paulosergio.games1997@gmail.com>
2025-01-13 23:42:21 -0300

    comentando e documentando a classe Main

--- src/servidor/Main.java
+++ src/servidor/Main.java
@@ -2,24 +2,38 @@ package servidor;
 
 import javax.swing.*;
 
-// Classe principal que inicia o servidor.
+/**
+ * Classe principal responsável por iniciar o servidor de chat.
+ * <p>
+ * Esta classe:
+ * <ul>
+ *     <li> Cria um objeto ConfiguradorServidor para obter a porta do servidor.</li>
+ *     <li> Inicia o servidor com a porta configurada, caso seja válida.</li>
+ * </ul>
+*/
 public class Main {
 
+    /**
+     * Ponto de entrada do programa.
+     * Inicia o processo de configuração e inicialização do servidor.
+     * @param args Argumentos de linha de comando (não utilizados neste caso).
+     */
     public static void main(String[] args) {
 
         // Cria um novo objeto da classe ConfiguradorServidor.
         ConfiguradorServidor configuradorServidor = new ConfiguradorServidor();
 
-        // Vai obter a porta que o responsável pelo servidor digitar.
+        // Irá obter a porta ao qual o responsável pelo servidor digitar.
         int porta = configuradorServidor.obterPorta();
 
-        // Se a porta escolhida for uma porta válida:
+        // Se a porta escolhida for uma porta válida (diferente de -1).
         if (porta != -1) {
 
-            // Cria um objeto da classe Servidor, passando a porta como parâmetro.
+            // Cria um objeto da classe Servidor, passando a porta como parâmetro e inicia o mesmo.
             Servidor servidor = new Servidor(porta);
-            servidor.iniciar(); // Inicia o servidor.
-        } else {    // Se a porta escolhida for inválida:
+            servidor.iniciar();
+
+        } else {    // Caso a porta escolhida for inválida exibe uma mensagem de erro.
             JOptionPane.showMessageDialog(null, "Servidor não iniciado. A configuração foi " +
                     "cancelada.");
         }
