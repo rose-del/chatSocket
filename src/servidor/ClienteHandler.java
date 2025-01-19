cd6476fe4d09c29aa24bc41a4877f5532f4b1c1d
Paulo <paulosergio.games1997@gmail.com>
2025-01-18 23:40:57 -0300

    documentacao da classe Servidor e ClienteHandler

--- src/servidor/ClienteHandler.java
+++ src/servidor/ClienteHandler.java
@@ -4,23 +4,67 @@ import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
-// Classe que gerencia a comunicação com um cliente
+/**
+ * <b>ClienteHandler</b>
+ * <p>
+ * Classe responsável por gerenciar a comunicação entre o servidor e um cliente especifico.
+ * <p>
+ * Implementa a interface {@code Runnable} para que cada instância possa ser executada em uma thread separada.
+ *
+ * <h3>Responsabilidades principais:</h3>
+ * <ul>
+ *     <li> Gerenciar a conexão com um cliente por meio de um {@code Socket}.</li>
+ *     <li> Receber mensagens do cliente e envia-las para todos os outros clientes conectados.</li>
+ *     <li> Encerrar a conexão e limpar os recursos quando o cliente se desconectar.</li>
+ * </ul>*/
 public class ClienteHandler implements Runnable {
 
+    /**
+     * Socket que representa a conexão com o cliente.
+     */
     private Socket conexao;
+
+    /**
+     * Lista compartilhada de {@code BufferedWriter} representa os clientes conectados.
+     * Cada {@code BufferedWriter} é usado para enviar mensagens para o respectivo cliente
+     */
     private ArrayList<BufferedWriter> clientes;
+
+    /**
+     * Fluxo de leitura de dados enviados pelo cliente.
+     */
     private BufferedReader leitorDados;
+
+    /**
+     * Fluxo de escrita para enviar dados ao cliente.
+     */
     private BufferedWriter escreveDados;
+
+    /**
+     * Nome do cliente, fornecido quando ele se conecta ao servidor.
+     */
     private String nome;
 
+    /**
+     * Contrutor da classe {@code ClienteHandler}.
+     * <p>
+     * Inicializa os fluxos de entrada e saída para o cliente e adiciona o fluxo de escrita à lista de clientes
+     * conectados.
+     *
+     * @param conexao Socket representando a conexão com o cliente.
+     * @param clientes Lista compartilhada de {@code BufferedWriter} dos clientes conectados.
+     */
     public ClienteHandler(Socket conexao, ArrayList<BufferedWriter> clientes) {
         this.conexao = conexao;
         this.clientes = clientes;
 
         try {
+
+            // Inicializa os fluxos de entrada e saída do cliente.
             this.leitorDados = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
             this.escreveDados = new BufferedWriter(new OutputStreamWriter(conexao.getOutputStream()));
 
+            // Adiciona o fluxo de escrita do cliente a lista de clientes conectados.
             synchronized (clientes) {
                 this.clientes.add(escreveDados);
             }
@@ -30,21 +74,37 @@ public class ClienteHandler implements Runnable {
         }
     }
 
+    /**
+     * Método executado quando a thread associada a esta instância é iniciada.
+     * <p>
+     * Gerencia a comunicação com o cliente:
+     * <ul>
+     *     <li> Lê o nome do cliente.</li>
+     *     <li> Processa mensagens para todos os outros clientes conectados.</li>
+     *     <li> Envia as mensagens para todos os outros clientes conectados.</li>
+     *     <li> Remove o cliente da lista e encerra a conexão quando ele se desconectar.</li>
+     * </ul>
+     */
     @Override
     public void run() {
         try {
-            nome = leitorDados.readLine();
+
+            // Lê o nome do cliente.
+            this.nome = leitorDados.readLine();
             String msg;
 
+            // Processa mensagens enviadas pelo cliente.
             while ((msg = leitorDados.readLine()) != null && !msg.equalsIgnoreCase("Sair")) {
                 enviarParaTodos(nome + " -> " + msg);
                 System.out.println(conexao.getInetAddress().getHostAddress() + " (" + nome + ") -> " + msg);
             }
 
+            // Remove o cliente da lista de clientes conectados.
             synchronized (clientes) {
                 clientes.remove(escreveDados);
             }
 
+            // Encerra fluxos e a conexão com o cliente.
             try {
                 leitorDados.close();
                 escreveDados.close();
@@ -58,11 +118,18 @@ public class ClienteHandler implements Runnable {
         }
     }
 
+    /**
+     * Envia mensagens para todos os clientes conectados, exceto o cliente que enviou a mensagem.
+     *
+     * @param mensagem A mensagem a ser enviada.
+     */
     private void enviarParaTodos(String mensagem) {
 
         synchronized (clientes) {
             for (BufferedWriter cliente : clientes) {
                 try {
+
+                    // Não envia a mensagem para o próprio cliente.
                     if (cliente != escreveDados) {
                         cliente.write(mensagem + "\r\n");
                         cliente.flush();
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -7,31 +7,78 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
+/**
+ * <b>Servidor</b>
+ * <p>
+ * Classe responsável por gerenciar as conexões e comunicações entre clientes em um servidor de chat simples.
+ * <p>
+ * <i>Responsabilidades:</i>
+ * <ul>
+ *     <li> Inicializar e configurar o servidor na porta especificada.</li>
+ *     <li> Aguardar conexões de clientes.</li>
+ *     <li> Gerenciar uma lista de clientes conectados.</li>
+ *     <li> Delegar o tratamento das conexões para uma thread separada.</li>
+ * </ul>
+ * <p>
+ * Esta classe utiliza a classe auxiliar {@link ClienteHandler} para gerenciar as iterações individuais com
+ * cada cliente conectado.*/
 class Servidor {
 
-    private int porta;
+    /**
+     * Porta na qual o servidor será iniciado.
+     */
+    private final int porta;
+
+    /**
+     * Socket principal do servidor, responsável por aceitar conexões.
+     * */
     private ServerSocket servidorFinal;
+
+    /**
+     * Lista de buffers para gerenciar os clientes conectados e facilitar a comunicação.
+     * */
     private ArrayList<BufferedWriter> clientes;
 
+    /**
+     * Construtor da classe Servidor.
+     * @param porta Porta na qual o servidor será iniciado.*/
     public Servidor(int porta) {
         this.porta = porta;
         this.clientes = new ArrayList<>();
     }
 
+    /**
+     * Inicia o servidor, permitindo conexões de clientes e gerenciando a comunicação.
+     * <p>
+     * O método:
+     * <ul>
+     *     <li> Inicializa o socket do servidor na porta especificada.</li>
+     *     <li> Exibe uma mensagem indicando que o servidor foi iniciado com sucesso.</li>
+     *     <li> Aguarda conexões de clientes de forma contínua.</li>
+     *     <li> Cria uma nova thread para cada cliente conectado, delegando a interação ao {@link ClienteHandler}.</li>
+     * </ul>*/
     public void iniciar() {
         try {
+
+            // Inicializa o socket do servidor na porta especificada.
             servidorFinal = new ServerSocket(porta);
             JOptionPane.showMessageDialog(null, "Servidor iniciado na porta: " + porta);
 
+            // Aguarda conexões de clientes enquanto o servidor estiver ativo.
             while (!servidorFinal.isClosed()) { // Verifica se o servidor não foi fechado
                 System.out.println("Aguardando conexão...");
+
+                // Aceita a conexão do cliente.
                 Socket conexao = servidorFinal.accept();
                 System.out.println("Cliente conectado...");
 
+                // Cria e inicia uma thread para lidar com o cliente conectado.
                 Thread clienteThread = new Thread(new ClienteHandler(conexao, clientes));
                 clienteThread.start();
             }
         } catch (IOException e) {
+
+            // trata erros ao inicializar o servidor ou aceitar conexões.
             JOptionPane.showMessageDialog(null, "Erro ao iniciar o servidor: " + e.getMessage());
         }
     }
